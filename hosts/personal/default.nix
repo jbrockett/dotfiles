0d33a8157f3767ad96715b75864d3ed3782fd976
Jeremy Brockett <jbrockett@users.noreply.github.com>
2025-01-29 14:37:35 -0500

    Add state version

--- hosts/personal/default.nix
+++ hosts/personal/default.nix
@@ -3,11 +3,16 @@
 {
   imports = [ ../../home/shared.nix ];
 
+  nixpkgs.config.allowUnfree = true;
+
   home = {
     username = "jeremy";
     homeDirectory = "/Users/jeremy";
+    stateVersion = "24.11";
     packages = with pkgs; [
       # Personal-specific packages
     ];
   };
+
+  home.enableNixpkgsReleaseCheck = false;
 }
