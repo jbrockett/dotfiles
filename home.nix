5c3f25b9ff73c2b4e756fbefb2e31bb70ab962de
Jeremy Brockett <313937+jbrockett@users.noreply.github.com>
2025-02-27 09:05:37 -0500

    Remove duplicate gc cofigurations

--- home.nix
+++ home.nix
@@ -25,11 +25,6 @@ in
     settings = {
       experimental-features = ["nix-command" "flakes"];
     };
-    gc = {
-      automatic = true;
-      frequency = "weekly";
-      options = "--delete-older-than 30d";
-    };
     package = pkgs.nix;
   };
 }
